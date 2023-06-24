99513a37c198ee71feaa159259f6af2b1d978853
“Gina” <Ginzywhisky1997@gmail.com>
2023-06-24 14:46:39 +0200

    my commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var accordions = document.getElementsByClassName("accordion");
+
+for (var i = 0; i < accordions.length; i++) {
+  accordions[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+  document.querySelector('.toggle-menu').addEventListener('click', function() {
+    var menuList = document.querySelector('.menu-list');
+    menuList.classList.toggle('show');
+  });
